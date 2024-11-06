71756026fabf7763c81ca554568cad35d791a177
топ <@DESKTOP-2P61BTM>
2024-11-06 10:57:41 +0200

    Добавьте файлы проекта.

---
+++ Lz_7.cpp
@@ -0,0 +1,35 @@
+﻿// Створити стек цілих чисел. Обчислити суму парних елементів стеку.//
+
+#include <iostream>
+#include <stack>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    stack<int>s;
+    int n, sum = 0;
+    cout << "Введіть розмір стеку: ";
+    cin >> n;
+    cout << "Введіть " << n << " цілих чисел: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        s.push(num);
+    }
+    while (!s.empty())
+    {
+        int num = s.top();
+        if (num %2 == 0) 
+        {
+            sum += num;
+        }
+        s.pop();
+    }
+    cout << "Сума парних елементів стеку: " << sum << endl;
+    return 0;
+}
